f3b4021422b4c3df5ecfb619c367f71a269ab343
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2015-03-03 20:07:46 +0100

    interesting (converge to a fun pattern with 3 agents)

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from numpy.linalg import norm
-from numpy.random import rand
+from numpy.random import rand, randn
 from explauto.utils.observer import Observable
 from sklearn.mixture import sample_gaussian
 from numpy import array, ones, hstack, tanh, argmax
@@ -13,14 +13,14 @@ class Agent(Observable):
         self.n_ag = gmm.n_components
         self.mean, self.covar = mean, covar
         self.gmm = gmm
-        self.presence = (1. / self.n_ag) * ones(self.n_ag)
-        self.lr = 0.1  # learning rate
-        self.weights = array([0.0] * (self.n_ag + 1))
+        self.presence = (1.) * ones(self.n_ag)
+        self.lr = 0.01  # learning rate
+        self.weights = array([0.0] * (2 * self.n_ag + 1))
         self.last_time_identified = [0] * self.n_ag
         self.t = 0
 
     def produce(self):
-        self.activation = self.weights.dot(hstack((1., self.presence)).T)
+        self.activation = self.weights.dot(hstack((1., self.presence, 1. - self.presence)).T)
         self.activation = (1. + tanh(self.activation * 8)) / 2.  # in [0, 1]
         if self.activation > rand():
             self.m = sample_gaussian(self.mean, self.covar)
@@ -41,7 +41,7 @@ class Agent(Observable):
             ag_id = argmax(probas)
             for i in range(self.n_ag):
                 if i == ag_id:
-                    self.presence[i] += 4.
+                    self.presence[i] += 1.
                 else:
                     self.presence[i] -= 0.1
         else:
@@ -49,9 +49,17 @@ class Agent(Observable):
         self.presence[self.presence > 1] = 1
         self.presence[self.presence < 0] = 0
         self.emit("presence", (self.id, deepcopy(self.presence)))
-        self.presence_gradient = norm(self.presence) - norm(prev_presence)
+        #self.presence_gradient = float(self.presence > 0.6  - prev_presence > 0.6)
+        #print self.presence_gradient
+        if all(self.presence > 0.6) > 0:
+            self.presence_gradient = 1
+        elif sum(self.presence > 0.6) - sum(prev_presence > 0.6) > 0:
+            self.presence_gradient = 1
+        else:
+            self.presence_gradient = -1
+
         self.emit("gradient", (self.id, self.presence_gradient))
-        self.weights += self.lr * (self.presence_gradient - 0.1) * \
-            hstack((1., prev_presence)) * (-1 if self.m is None else 1)
+        self.weights += self.lr * (self.presence_gradient) * \
+            hstack((1., self.presence, 1. - prev_presence)) * (-1 if self.m is None else 1)
         self.emit("weights", (self.id, self.weights))
         self.t += 1
